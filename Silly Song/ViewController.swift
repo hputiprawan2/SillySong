ed6fee4f01a7468c5fe465732ad225798f96dff9
Hanna Putiprawan <hanna.p@apple.com>
2021-01-06 03:00:02 -0800

    [Add] Silly Song App - Getting name from the user, and display the name back in the lyrics

--- Silly Song/ViewController.swift
+++ Silly Song/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameField: UITextField!
+    
+    @IBOutlet weak var lyricsView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        nameField.delegate = self
+        nameField.returnKeyType = .done
     }
 
-
+    // Clear out the text of both the nameField and the lyricsView
+    @IBAction func reset(_ sender: Any) {
+        nameField.text = ""
+        lyricsView.text = ""
+    }
+    
+    // Take the name entered in the name field, generate the personalized lyrics, and display the lyrics in the lyrics view
+    @IBAction func displayLyrics(_ sender: AnyObject) {
+        let fullName = nameField.text!.capitalized
+        let shortName = fullName.dropFirst()
+        let lyrics = "\(fullName), \(fullName), Bo B\(shortName) Banana Fana Fo F\(shortName) Me My Mo M\(shortName) \(fullName) ♡"
+        lyricsView.text = lyrics
+    }
 }
 
+// MARK: - UITextFieldDelegate
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return false
+    }
+}
